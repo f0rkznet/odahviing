68802a45ac79b63ee87873d9dfdcf7f04179f62c
f0rkz <f0rkz@f0rkznet.net>
2021-03-30 15:51:42 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import board
+import neopixel
+import random
+
+pixel_pin = board.D18
+num_pixels = 12
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin,
+    num_pixels,
+    auto_write=False,
+    pixel_order=ORDER,
+)
+
+heatmap = {
+    'low': [(0,0,0), (0,255,0)],
+    'medium': [(0,0,0), (0,0,255)],
+    'high': [(0,0,0), (255,0,0)],
+}
+
+def fire_cycle(heat):
+    for pixel in range(num_pixels):
+        pixels[pixel] = random.choice(heat)
+        pixels.show()
+        time.sleep(random.choice(0, 0.25, 0.5, 0.75))
+
+if __name__ == '__main__':
+    while True:
+        fire_cycle(heat=heatmap['low'])
