05c453dcf06b6fa8fdc492377116d14a14729222
f0rkz <f0rkz@f0rkznet.net>
2021-03-30 16:45:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ apt install -y python3 python3-pip git
 
 pip3 install -r requirements.txt
 
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+sudo python3 -m pip install --force-reinstall adafruit-blinka
+
 cp torch.service /usr/lib/systemd/system/torch.service
 systemctl enable torch.service
 systemctl daemon-reload
-systemctl start torch.service
+systemctl restart torch.service
