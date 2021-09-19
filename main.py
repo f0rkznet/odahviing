1c9af91e0834eca25c8ba5d059173d4551e2ce68
f0rkz <f0rkz@f0rkznet.net>
2021-09-19 17:21:05 -0400

    message

--- main.py
+++ main.py
@@ -158,6 +158,7 @@ if __name__ == '__main__':
         #in_rate = get_port_heat(LIBRENMS_PORT)
         # Moving to netdata to get the info
         if_rate = get_rate('10.250.1.1:19999', 'net.eth8')
+        print(if_rate)
         for _ in range(1):
             if if_rate <= 25:
                 green_cycle()
